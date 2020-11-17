40654a149805a45d9bc1aa9153a17853be8fa905
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2020-11-18 02:24:45 +0300

    Добавьте файлы проекта.

---
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using WorkingWithVisualStudio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
+    }
+}
